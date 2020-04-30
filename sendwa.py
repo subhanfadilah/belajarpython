76dfac99b969b990799bdda76e88d9b61cac0ef6
subhanfadilah <63758109+subhanfadilah@users.noreply.github.com>
2020-04-30 20:51:16 +0700

    Add files via upload

---
+++ sendwa.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import time
+
+os.system('clear')
+print "[1] Chat Admin"
+pilih = raw_input('[?] Masukkan Pilihan anda : ')
+if pilih == '1':
+	os.system('clear')
+	os.system('termux-open-url https://api.whatsapp.com/send?phone=+6285225218341&text=Info-sumandingtools')
+else:
+	print "Pilihan Anda Error gans"
